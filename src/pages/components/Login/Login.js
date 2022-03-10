3b8380db61fd84b79e027aa85e98fee40c0b4c4f
MohammadMahdiKaseb <mohammadmahdikaseb@gmail.com>
2022-03-10 20:37:06 +0330

    sign in with email completed

--- src/pages/components/Login/Login.js
+++ src/pages/components/Login/Login.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css"
+import Swal from "sweetalert2";
 import { auth, provider } from "../../../config/firebaseConfig";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup , signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 function Login(props) {
   let navigate = useNavigate();
+  const [email , setEmail] = useState("")
+  const [password, setPassowrd] = useState("")
+  
   const signInGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem("isAuth", true);
@@ -16,8 +20,36 @@ function Login(props) {
       navigate("/");
     });
   };
+
+  const loginWithEmail = () => {
+    signInWithEmailAndPassword(auth, email, password).then((result) => {
+      localStorage.setItem("isAuth", true);
+      props.setIsAuth(true);
+      Swal.fire({
+        title: "Welcome Back!",
+        text: "You sign in",
+        icon: "success",
+        timer: 3000
+      })
+      console.log(result)
+    }).catch((err) => {
+      console.log(err)
+      Swal.fire({
+        title: "Opps...",
+        text: err,
+        icon: "error",
+        timer: 3000
+      })
+    })
+    
+  }
+
+
   return (
     <div className="loginPage">
+      <input type="email" placeholder="Email..." onChange={(event) => { setEmail(event.target.value) }}/>
+      <input type="password" placeholder="Password..." onChange={(event) => { setPassowrd(event.target.value)}}/>
+      <button onClick={loginWithEmail}>Login</button>
       <button type="button" onClick={signInGoogle} class="login-with-google-btn">
         Sign in with Google
       </button>
